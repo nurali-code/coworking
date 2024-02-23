fa8be67e62459499a623c40b64b4c9f588d4846c
nurali-code <nur3.dav.97@gmail.com>
2024-02-23 16:52:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Для modal
+document.querySelectorAll('.modal_toggle').forEach(function (modalOpen) {
+    modalOpen.addEventListener('click', function () {
+        document.getElementById('modal').classList.toggle('is_active');
+    });
+});
+
+// Для phoneMask
+const phones = document.querySelectorAll('.phone');
+let phoneMask = new Inputmask("+7 (999)-999-99-99");
+phones.forEach(phone => {
+    phoneMask.mask(phone);
+});
+
+function sendJSON() {
+    event.preventDefault();
+    let name = document.querySelector('#name');
+    let phone = document.querySelector('#phone');
+    let city = document.querySelector('#city');
+    var settings = {
+        "url": "https://backend.sdelkaservicedomain.ru/sdelka/leadsForLanding/leadFromLanding",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "phone": phone.value,
+            "name": name.value,
+            "city": city.value,
+        }),
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#author", {
+    scrollTrigger: {
+        trigger: "#authorWrap",
+        start: "80% 80%",
+        end: "100% 80%",
+        // markers: true,
+        scrub: true
+    },
+    filter: "blur(0px)"
+});
+
